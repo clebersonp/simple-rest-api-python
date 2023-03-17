054006a0602bb8d9ab7e3590dad722ab51528eac
Cleberson <pauluci.cleberson@gmail.com>
2023-03-17 20:31:15 -0300

    Criado app rest api python

---
+++ app.py
@@ -0,0 +1,1 @@
+# app.pyfrom fastapi import FastAPIapp = FastAPI()@app.get("/")async def root(name = None):		if name is None:		text = 'World'	else:		text = name	return {"message": "Hello " + text + "!"}
