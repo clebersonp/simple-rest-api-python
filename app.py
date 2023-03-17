6e58b8208548fdbe6b4f37e88f6b2d3ab2b151fe
Cleberson <pauluci.cleberson@gmail.com>
2023-03-17 20:52:41 -0300

    Corrigida identacao

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root(name = None):
-
+	
 	if name is None:
 		text = 'World'
 	else:
 		text = name
-		
-return {"message": "Hello" + text + "!"}
+	return {"message": "Hello " + text + "!"}
