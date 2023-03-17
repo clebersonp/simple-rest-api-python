a8a8f4953a4a25ba3185571e43398327fa7d856f
Cleberson <pauluci.cleberson@gmail.com>
2023-03-17 20:42:12 -0300

    Corrigido encoding

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-# app.pyfrom fastapi import FastAPIapp = FastAPI()@app.get("/")async def root(name = None):		if name is None:		text = 'World'	else:		text = name	return {"message": "Hello " + text + "!"}
+# app.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root(name = None):
+
+	if name is None:
+		text = 'World'
+	else:
+		text = name
+		
+return {"message": "Hello" + text + "!"}
